c9a490443f095f344863b4e01dfad606d54cf28f
rajan7227 <114588581+rajan7227@users.noreply.github.com>
2023-07-15 11:54:39 +0100

    "shows section through javascript "

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -48,21 +48,44 @@ const shows = [
     
       const showContainer = document.querySelector(".shows");
 
+      const heading =document.createElement("h2")
+      heading.classList.add("heading");
+      heading.innerText = "SHOWS"
+      showContainer.appendChild(heading);
+
       
     
       shows.forEach((show) => {
         const party__wrapper = document.createElement("div");
         party__wrapper.classList.add("party__wrapper");
-      
+
+        const heading__date =document.createElement("h3")
+        heading__date.classList.add("heading__date");
+        heading__date.innerText = "DATE"
+        party__wrapper.appendChild(heading__date);
+        console.log(heading__date);
+
         const party__date = document.createElement("p");
         party__date.classList.add("party__date");
         party__date.innerText = show.date;
         party__wrapper.appendChild(party__date);
+
+        const heading__venue =document.createElement("h3")
+        heading__venue.classList.add("heading__venue");
+        heading__venue.innerText = "VENUE"
+        party__wrapper.appendChild(heading__venue);
+        console.log(heading__venue);
       
         const party__venue = document.createElement("p");
         party__venue.classList.add("party__venue");
         party__venue.innerText = show.venue;
         party__wrapper.appendChild(party__venue);
+
+        const heading__location =document.createElement("h3")
+        heading__location.classList.add("heading__location");
+        heading__location.innerText = "LOCATION"
+        party__wrapper.appendChild(heading__location);
+        console.log(heading__location);
       
         const party__location = document.createElement("p");
         party__location.classList.add("party__location");
