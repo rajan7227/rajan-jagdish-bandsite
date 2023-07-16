8f7e8a7a405e03707c5b7dde543c2ada8a319f28
rajan7227 <114588581+rajan7227@users.noreply.github.com>
2023-07-16 20:52:36 +0100

    "added responsive design"

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -48,49 +48,56 @@ const shows = [
     
       const showContainer = document.querySelector(".shows");
 
-      const heading =document.createElement("h2")
-      heading.classList.add("heading");
-      heading.innerText = "SHOWS"
-      showContainer.appendChild(heading);
+const heading = document.createElement("h2");
+heading.classList.add("heading");
+heading.innerText = "Shows";
+showContainer.appendChild(heading);
 
-      
-    
-      shows.forEach((show) => {
-        const party__wrapper = document.createElement("div");
-        party__wrapper.classList.add("party__wrapper");
+shows.forEach((show) => {
+  const party__wrapper = document.createElement("div");
+  party__wrapper.classList.add("party__wrapper");
 
-        const heading__date =document.createElement("h3")
-        heading__date.classList.add("heading__date");
-        heading__date.innerText = "DATE"
-        party__wrapper.appendChild(heading__date);
-        console.log(heading__date);
+  // Date
+  const dateDiv = document.createElement("div");
+  dateDiv.classList.add("party__date");
+  const heading__date = document.createElement("h3");
+  heading__date.innerText = "DATE";
+  dateDiv.appendChild(heading__date);
+  const party__date = document.createElement("p");
+  party__date.classList.add("para__bold")
+  party__date.innerText = show.date;
+  dateDiv.appendChild(party__date);
+  party__wrapper.appendChild(dateDiv);
 
-        const party__date = document.createElement("p");
-        party__date.classList.add("party__date");
-        party__date.innerText = show.date;
-        party__wrapper.appendChild(party__date);
+  // Venue
+  const venueDiv = document.createElement("div");
+  venueDiv.classList.add("party__venue");
+  const heading__venue = document.createElement("h3");
+  heading__venue.innerText = "VENUE";
+  venueDiv.appendChild(heading__venue);
+  const party__venue = document.createElement("p");
+  party__venue.innerText = show.venue;
+  venueDiv.appendChild(party__venue);
+  party__wrapper.appendChild(venueDiv);
 
-        const heading__venue =document.createElement("h3")
-        heading__venue.classList.add("heading__venue");
-        heading__venue.innerText = "VENUE"
-        party__wrapper.appendChild(heading__venue);
-        console.log(heading__venue);
-      
-        const party__venue = document.createElement("p");
-        party__venue.classList.add("party__venue");
-        party__venue.innerText = show.venue;
-        party__wrapper.appendChild(party__venue);
+  // Location
+  const locationDiv = document.createElement("div");
+  locationDiv.classList.add("party__location");
+  const heading__location = document.createElement("h3");
+  heading__location.innerText = "LOCATION";
+  locationDiv.appendChild(heading__location);
+  const party__location = document.createElement("p");
+  party__location.innerText = show.location;
+  locationDiv.appendChild(party__location);
+  party__wrapper.appendChild(locationDiv);
 
-        const heading__location =document.createElement("h3")
-        heading__location.classList.add("heading__location");
-        heading__location.innerText = "LOCATION"
-        party__wrapper.appendChild(heading__location);
-        console.log(heading__location);
-      
-        const party__location = document.createElement("p");
-        party__location.classList.add("party__location");
-        party__location.innerText = show.location;
-        party__wrapper.appendChild(party__location);
-      
-        showContainer.appendChild(party__wrapper);
-      });
+  // Button
+  const buttonDiv = document.createElement("div");
+  buttonDiv.classList.add("party__button");
+  const heading__button = document.createElement("button");
+  heading__button.innerText = "BUY TICKETS";
+  buttonDiv.appendChild(heading__button);
+  party__wrapper.appendChild(buttonDiv);
+
+  showContainer.appendChild(party__wrapper);
+});
