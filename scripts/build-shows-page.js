513e430d03b29d4e4476ef2b533febc4f557239c
rajan7227 <114588581+rajan7227@users.noreply.github.com>
2023-07-22 21:24:36 +0100

    Shows page now loading through API database

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,35 +1,3 @@
-// const shows = [
-//   {
-//     date: "Mon Sep 06 21",
-//     venue: "Ronald Lane",
-//     location: "San Francisco",
-//   },
-//   {
-//     date: "Tue Sept 21 2021",
-//     venue: "Pier 3 East",
-//     location: "San Francisco",
-//   },
-//   {
-//     date: "Fri Oct 15 2021",
-//     venue: "View Lounge",
-//     location: "San Francisco",
-//   },
-//   {
-//     date: "Sat Nov 06 2021",
-//     venue: "Hyatt Agency",
-//     location: "San Francisco",
-//   },
-//   {
-//     date: "Fri Nov 26 2021",
-//     venue: "Moscow Center",
-//     location: "San Francisco",
-//   },
-//   {
-//     date: "Wed Dec 15 2021",
-//     venue: "Press Club",
-//     location: "San Francisco",
-//   },
-// ];
 
 const API_KEY = "?api_key=89205fa9-50a7-45c3-b47f-ee0059378a3c";
 const API_URL = "https://project-1-api.herokuapp.com";
@@ -56,6 +24,27 @@ const getShows = ()=>{
     heading.classList.add("heading");
     heading.innerText = "Shows";
     showContainer.appendChild(heading);
+
+    const singleHeading = document.createElement("div");
+    singleHeading.classList.add("singleHeading")
+    showContainer.appendChild(singleHeading);
+
+    const date = document.createElement("h3");
+    date.classList.add("singleheading__date");
+    date.innerText= "DATE";
+    singleHeading.appendChild(date)
+
+    const venue = document.createElement("h3");
+    venue.classList.add("singleheading__venue");
+    venue.innerText= "VENUE";
+    singleHeading.appendChild(venue)
+
+    const location = document.createElement("h3");
+    location.classList.add("singleheading__date");
+    location.innerText= "LOCATION";
+    singleHeading.appendChild(location)
+
+
     
     showDetails.forEach((show) => {
       const party__wrapper = document.createElement("div");
@@ -65,11 +54,19 @@ const getShows = ()=>{
       const dateDiv = document.createElement("div");
       dateDiv.classList.add("party__date");
       const heading__date = document.createElement("h3");
+      heading__date.classList.add("h3");
       heading__date.innerText = "DATE";
       dateDiv.appendChild(heading__date);
+
+      //converting timestamp data into readable format
+      const dateObject = new Date(show.date);
+      console.log(dateObject)
+      const formattedDate = dateObject.toDateString();
+      console.log(formattedDate)
+
       const party__date = document.createElement("p");
       party__date.classList.add("para__bold")
-      party__date.innerText = show.date;
+      party__date.innerText = formattedDate;
       dateDiv.appendChild(party__date);
       party__wrapper.appendChild(dateDiv);
     
@@ -77,6 +74,7 @@ const getShows = ()=>{
       const venueDiv = document.createElement("div");
       venueDiv.classList.add("party__venue");
       const heading__venue = document.createElement("h3");
+      heading__venue.classList.add("h3");
       heading__venue.innerText = "VENUE";
       venueDiv.appendChild(heading__venue);
       const party__venue = document.createElement("p");
@@ -88,6 +86,7 @@ const getShows = ()=>{
       const locationDiv = document.createElement("div");
       locationDiv.classList.add("party__location");
       const heading__location = document.createElement("h3");
+      heading__location.classList.add("h3");
       heading__location.innerText = "LOCATION";
       locationDiv.appendChild(heading__location);
       const party__location = document.createElement("p");
@@ -109,9 +108,11 @@ const getShows = ()=>{
 
 
   })
-  .catch((error)={
-  
-  })
+  .catch((error)=>{
+    console.error("Error fetching show details:", error);
+    console.error("Error response data:", error.response.data);
+    console.error("Error status:", error.response.status);
+  });
 }
 
 getShows();
